fa7ce859af9aeb94bcc7742fb8d3d82cd1d04bb7
kjkb <98828564+kjkb@users.noreply.github.com>
2022-04-27 05:39:25 +0000

    修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -29,7 +29,7 @@ class Public::SessionsController < Devise::SessionsController
   def reject_deleted_user
     @user = User.find_by(name: params[:user][:name])
     if @user
-      if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == true)
+      if @user.valid_password?(params[:user][:password]) && @user.is_deleted == true
         flash[:alert] = "退会済みです。再度ご登録をしてご利用ください。"
         redirect_to new_user_session_path
       end
